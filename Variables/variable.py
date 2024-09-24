023a8d9d536812caced88dcdf9028b715eb865ed
Shru143 <20bcg1121@cuchd.in>
2024-09-24 22:55:43 +0530

    ninth commit

---
+++ Variables/variable.py
@@ -0,0 +1,11 @@
+#A variable in Python is like a box where you can store a value, such as a number or a word, so you can use it later. You give the box a name, and every time you use that name, Python knows which value you mean.
+
+
+name = "Shruti"  # Storing the word "Shruti" in the variable 'name'
+age = 22         # Storing the number 22 in the variable 'age'
+
+print(name)  # This will print "John"
+print(age)   # This will print 25
+
+
+#You can change what's inside the box anytime, and Python will remember the new value.

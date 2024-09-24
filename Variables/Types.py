e7487621db8e7b3961b29bfa500e99ab47218b41
Shru143 <20bcg1121@cuchd.in>
2024-09-24 22:57:49 +0530

    ninth commit

---
+++ Variables/Types.py
@@ -0,0 +1,21 @@
+#INTEGER
+#This type of variable stores whole numbers, without any decimals.
+
+#Example
+age = 22             #22 is a number (integer)
+print(age)           #output = 22
+
+#Example 2
+temperature = -5
+print(temperature)  #output = -5
+
+#STRING
+#A string stores text (words or sentences). Strings are always written inside quotes.
+
+#Example1
+name = "Shruti Chadwal"
+print(name)                #output = Shruti Chadwal
+
+#Example 2
+poem = "Roses are Red"
+print(poem)
